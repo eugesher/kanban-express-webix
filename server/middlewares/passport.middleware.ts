148c0c483218176e1de9129477e7fb5149def4d4
Evgenii Chernyshov <mailbox@eugesher.site>
2021-10-25 17:50:03 +0300

    fix: session

--- server/middlewares/passport.middleware.ts
+++ server/middlewares/passport.middleware.ts
@@ -3,28 +3,35 @@ import { Strategy as LocalStrategy } from 'passport-local';
 import { getRepository } from 'typeorm';
 import { compare } from 'bcrypt';
 import User from '../entities/user.entity';
+import ISession from '../types/interfaces/session.interface';
 
 const INVALID_CREDENTIALS_MESSAGE = 'invalid user credentials';
 
 passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    const user = await getRepository(User).findOne(
-      { username },
-      { select: ['id', 'password'] },
-    );
+  new LocalStrategy(
+    { passReqToCallback: true },
+    async (req, username, password, done) => {
+      const user = await getRepository(User).findOne(
+        { username },
+        { select: ['id', 'password'] },
+      );
 
-    if (!user) {
-      return done(null, false, { message: INVALID_CREDENTIALS_MESSAGE });
-    }
+      if (!user) {
+        return done(null, false, { message: INVALID_CREDENTIALS_MESSAGE });
+      }
 
-    const isValidPassword = await compare(password, user.password);
+      const isValidPassword = await compare(password, user.password);
 
-    if (!isValidPassword) {
-      return done(null, false, { message: INVALID_CREDENTIALS_MESSAGE });
-    }
+      if (!isValidPassword) {
+        return done(null, false, { message: INVALID_CREDENTIALS_MESSAGE });
+      }
 
-    return done(null, user);
-  }),
+      const session = req.session as ISession;
+      session.user = user;
+
+      return done(null, user);
+    },
+  ),
 );
 
 passport.serializeUser((user, done) => {
